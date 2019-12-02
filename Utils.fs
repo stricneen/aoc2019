ba946f93a12c6a93fec8973c39b7f13c71726b2f
Paul Brown <paul.brown@bjss.com>
2019-12-02 06:57:05 +0000

    Fixed seq

--- Utils.fs
+++ Utils.fs
@@ -17,4 +17,4 @@ let printn text = printf  "%i\n" text
 let readCSV filePath = 
     let line = readLines filePath
                |> Seq.head
-    line.Split [|','|] |> Array.toSeq |> Seq.map Int32.Parse
+    line.Split [|','|] |> Array.map Int32.Parse |> Seq.toArray
