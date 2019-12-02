5785a84ef3de7d51085aa27645e85b10a232909e
Paul Brown <paul.brown@bjss.com>
2019-12-02 14:59:45 +0000

    Tidy up 2

--- Day2.fs
+++ Day2.fs
@@ -6,63 +6,50 @@ let day2 =
 
     print "Advent of code - Day 2"
 
+    let execute prog noun verb =     
+    
+        Array.set prog 1 noun
+        Array.set prog 2 verb
+
+        let tick inst = match inst with
+                                | [| 1; x; y; z |] -> Array.set prog z (prog.[x] + prog.[y])
+                                                      prog
+                                | [| 2; x; y; z |] -> Array.set prog z (prog.[x] * prog.[y])
+                                                      prog
+                                | _ -> prog
+
+        let instCount = prog |> Array.chunkBySize 4 |> Array.length
+        
+        // printfn "Chunks: %A" instCount
+
+        for var = 0 to instCount - 1 do
+            let inst = prog |> Array.chunkBySize 4 |> Array.skip var
+            let t = tick inst.[0]
+            ()
+    
+        prog.[0]
+
+
     let prog = readCSV "./data/day2.txt"
 
-    // Apply fix
-    Array.set prog 1 12
-    Array.set prog 2 2
-
-    let tick inst = match inst with
-                            | [| 1; x; y; z |] -> Array.set prog z (prog.[x] + prog.[y])
-                                                  prog
-                            | [| 2; x; y; z |] -> Array.set prog z (prog.[x] * prog.[y])
-                                                  prog
-                            | _ -> prog
-
-    let instCount = prog |> Array.chunkBySize 4 |> Array.length
-    
-    printfn "Chunks: %A" instCount
-
-
-    for var = 0 to instCount - 1 do
-        let inst = prog |> Array.chunkBySize 4 |> Array.skip var
-        let t = tick inst.[0]
-        ()
-        // printfn "Chunk: %A" inst
-        // printfn "Prog: %A" prog
-            
+    let f = execute prog 12 2
 
     printfn ""
-    printfn "Out: %A" prog   
+    printfn "Out: %A" f   
 
 
+    print "Part 2"
+
+    for noun = 0 to 99 do
+        for verb = 0 to 99 do
+            let prog = readCSV "./data/day2.txt"
+            let f = execute prog noun verb
+            if f = 19690720 then    
+                printn noun
+                printn verb
+                printn ((100 * noun) + verb)
 
 
-
-
-
-
-
-
-    // let inst = prog
-    //             |> Array.chunkBySize 4
-    //             |> Array.skip x
-    //             |> Array.take 1
-    
-                
-                
-    
-
-
-
-
-    //printfn "%A" out
-
-
-    // match inst.Head with
-    // | [| 1, x, y, z |] -> ()
-
-   
 
 
     ()
