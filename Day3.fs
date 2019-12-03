fd885603357cd133ccd3e4673e5741aeb3e8f6bb
Paul Brown <paul.brown@bjss.com>
2019-12-03 17:11:39 +0000

    Play

--- Day3.fs
+++ Day3.fs
@@ -1,16 +1,20 @@
 module Day3
 
 open Utils
-open System.Linq
 
 let day3 = 
     print "Advent of code - Day 3"
 
     let input = readLines "./data/day3.txt"
-    
+
     let wire1 = input |> Seq.head |> Utils.split
     let wire2 = input |> Seq.skip 1 |> Seq.head |> Utils.split
 
+    let test1 = "R5,U5"  //,D30,R83,U83,L12,D49,R71,U7,L72"
+    let test2 = "U62,R66,U55,R34,D71,R55,D58,R83"
+    
+    let wire1 = test1 |> Utils.split
+    let wire2 = test2 |> Utils.split
 
         //        (340, 430)  L43
     let route start vector = 
@@ -22,28 +26,42 @@ let day3 =
         | 'D' -> seq { for v in snd start .. -1 .. (snd start - snd inst) do (fst start, v) }
         | 'L' -> seq { for v in fst start .. -1 .. (fst start - snd inst) do (v, snd start) }
         | 'R' -> seq { for v in fst start .. (fst start + snd inst) do  (v, snd start) }
-        | _ -> seq { (0,0) }
+        | _ -> Seq.empty
 
     let s = seq { yield (0,0)}
 
+    // let x  = route (2,2) "R5" 
+    //          |> Seq.toList
+    // printf "%A" x
+
+
     let folder acc elem = 
         let start = acc |> Seq.last
+
+        printf "acc %A\n" (acc |> Seq.toList)
+        
         let r = route start elem
+        
+        printf "r %A\n" (r |> Seq.toList)
+
+        printf "out %A\n" (acc |> Seq.toList)
+
         acc 
-        |> Seq.append r
+    
 
     let coords wire =
         wire
         |> Seq.fold folder s
 
-    let y = route (0,0) "L43"
+    //let y = route (0,0) "L43"
     
-    let x = coords wire1
-    let y = coords wire2
+    let x = coords wire1 |> Seq.toList
+    printf "%A" x
+    
+    // let y = coords wire2
 
-    let cross = intersect x y
-
-    printf "%A" cross
+    // let cross = intersect x y
+    // printf "%A" cross
     
     //y |> Seq.iter (fun x -> printf "%a , %a" (fst x) (snd x))
     
@@ -51,7 +69,7 @@ let day3 =
 
 
 
-    print "end"
+
 
     //let coords1 = coords wire1
     //let coords1 = coords wire1
