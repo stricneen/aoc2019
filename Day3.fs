77a2b092d4a8b3b7dbe27f439bbf17bfc27d2032
Paul Brown <paul.brown@bjss.com>
2019-12-03 18:46:52 +0000

    Day 3 tidy

--- Day3.fs
+++ Day3.fs
@@ -10,13 +10,6 @@ let day3 =
     let wire1 = input |> Seq.head |> Utils.split
     let wire2 = input |> Seq.skip 1 |> Seq.head |> Utils.split
 
-    let test1 = "R5,U5"  //,D30,R83,U83,L12,D49,R71,U7,L72"
-    let test2 = "U62,R66,U55,R34,D71,R55,D58,R83"
-    
-    let wire1x = test1 |> Utils.split
-    let wire2x = test2 |> Utils.split
-
-        //        (340, 430)  L43
     let route start vector = 
         let splitv (dir:string) = 
             (dir.[0], dir.[1..] |> System.Int32.Parse)
@@ -30,62 +23,27 @@ let day3 =
 
     let s = seq { yield (0,0)}
 
-    // let x  = route (2,2) "R5" 
-    //          |> Seq.toList
-    // printf "%A" x
-
-
     let folder acc elem = 
         let start = acc |> Seq.last
-
-        //printf "acc %A\n" (acc |> Seq.toList)
-        
         let r = route start elem
-        
-        let rr = r |> Seq.skip 1
-
-        //printf "r %A\n" (r |> Seq.toList)
-
-       // printf "out %A\n" (acc |> Seq.toList)
-
-        Seq.append acc rr
+        Seq.append acc (r |> Seq.skip 1)
 
     let coords wire =
         wire
         |> Seq.fold folder s
 
-    //let y = route (0,0) "L43"
     
-    let x = coords wire1 |> Seq.toList
-    // printf "%A" x
-    
+    let x = coords wire1
     let y = coords wire2
 
     let cross = intersect x y
-    printf "%A" (cross |> Seq.toList)
-
+   
 
     let manhatten = cross   
                     |> Seq.skip 1
                     |> Seq.map (fun x -> abs (fst x) + abs (snd x))
                     |> Seq.min
     
-    printf "%A" manhatten
-    //y |> Seq.iter (fun x -> printf "%a , %a" (fst x) (snd x))
-    
-    //printn (y |> Seq.length)
-
-
-
-
-
-    //let coords1 = coords wire1
-    //let coords1 = coords wire1
-    
-
-
-
-
-
+    printf "%A\n" manhatten
 
     ()
