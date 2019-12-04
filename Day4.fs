e69bbc9a56c0424e4f4e639832bc4d11f1566939
Paul Brown <paul.brown@bjss.com>
2019-12-04 05:25:43 +0000

    Part 1

--- Day4.fs
+++ Day4.fs
@@ -4,4 +4,40 @@ open Utils
 
 let day4 = 
     print "Advent of code - Day 4"
+
+    let haspair number =
+        let s  = number.ToString()
+        s 
+        |> Seq.windowed 2
+        |> Seq.exists (fun x -> x.[0] = x.[1])
+
+
+    let inc number =
+        let s  = number.ToString()
+        s 
+        |> Seq.windowed 2
+        |> Seq.forall (fun x -> x.[0] <= x.[1])
+
+
+    let count = seq {367479 .. 893698}
+                |> Seq.filter haspair
+                |> Seq.filter inc
+                |> Seq.length
+
+    printn count
+
+    print "Part 2"
+
+    let count2 = seq {367479 .. 893698}
+                |> Seq.filter haspair
+                |> Seq.filter inc
+                |> Seq.length
+
+    printn count2
+    // for n in 367479 .. 893698 do
+    //     if haspair n && inc n then 
+    //         printn n
+
+
+
     ()
--- Program.fs
+++ Program.fs
@@ -5,7 +5,8 @@ let main argv =
 
     //Day2.day2 |> ignore
 
-    Day3.day3 |> ignore
+    //Day3.day3 |> ignore
 
+    Day4.day4 |> ignore
 
     0 // return an integer exit code
