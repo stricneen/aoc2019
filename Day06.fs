9931bb52361a72011a3c92c387a4a0674b69d7cd
Paul Brown <paul.brown@bjss.com>
2019-12-06 13:36:17 +0000

    Start day 6

--- Day06.fs
+++ Day06.fs
@@ -5,12 +5,14 @@ open Utils
 let day6 = 
     print "Advent of code - Day 6"
 
-    let input = readLines "./data/day1.txt"
+    let input = readLines "./data/day6.txt"
 
     let data = input
                |> Seq.map(fun x -> x.[0..2], x.[4..6])
 
+    let com = data |> Seq.filter(fun x -> fst x = "COM")
 
+    printf "%A\n" com
     printf "%A\n" data
 
 
