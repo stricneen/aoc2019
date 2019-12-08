e2a6a8854edaacc9b4237b19a9abb6aa0d7c7701
Paul Brown <paul.brown@bjss.com>
2019-12-08 10:09:18 +0000

    8 - 1

--- Day08.fs
+++ Day08.fs
@@ -5,8 +5,25 @@ open Utils
 let day8 = 
     print "Advent of code - Day 8 - Space Image Format"
 
-    let input = readLines "./data/day8.txt"
+    let input = (readLines "./data/day8.txt").[0]
+
+    let layer = 25 * 6
+
+    let count x = Seq.filter ((=) x) >> Seq.length // **
+
+    let layers image =
+        image
+        |> Seq.chunkBySize layer
+        |> Seq.map (fun x -> (x |> count '0' , x))
+        |> Seq.minBy fst
+        
+    let lowest = snd (layers input)
+
+    let ones = lowest |> Array.filter (fun x -> x = '1')  |> Array.length
+    let twos = lowest |> Array.filter (fun x -> x = '2')  |> Array.length
+    
 
 
+    printf "%A\n" (ones * twos)
 
     0
