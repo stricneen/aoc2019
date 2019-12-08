04c055cf6b891b240467a7f973f5010e37d610a9
Paul Brown <paul.brown@bjss.com>
2019-12-08 10:49:33 +0000

    Day 8

--- Day08.fs
+++ Day08.fs
@@ -7,23 +7,39 @@ let day8 =
 
     let input = (readLines "./data/day8.txt").[0]
 
-    let layer = 25 * 6
+    let pixels = 25 * 6
 
-    let count x = Seq.filter ((=) x) >> Seq.length // **
+    let count x = Seq.filter ((=) x) >> Seq.length // ** 40022846
 
     let layers image =
         image
-        |> Seq.chunkBySize layer
+        |> Seq.chunkBySize pixels
+
+    let photoLayers = layers input
+
+    let lowestZeroCount image =
+        image
         |> Seq.map (fun x -> (x |> count '0' , x))
         |> Seq.minBy fst
         
-    let lowest = snd (layers input)
+    let lowest = snd (lowestZeroCount photoLayers)
 
     let ones = lowest |> Array.filter (fun x -> x = '1')  |> Array.length
     let twos = lowest |> Array.filter (fun x -> x = '2')  |> Array.length
-    
-
 
     printf "%A\n" (ones * twos)
+
+    print "Part 2"
+
+    let photo = 
+        photoLayers
+        |> Seq.transpose
+        |> Seq.map (fun x -> x |> Seq.find (fun y -> y <> '2'))
+        |> Seq.chunkBySize 25
+        |> Seq.map System.String
+        // |> Seq.map (fun x -> x.Replace('1', '*'))
+        // |> Seq.map (fun x -> x.Replace('0', ' '))
+
+    photo |> Seq.iter (printf "%A\n")
 
     0
