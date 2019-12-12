20683ec286fd756653bba14ecdefaca2900354bd
Paul Brown <paul.brown@bjss.com>
2019-12-12 05:19:44 +0000

    Input

--- Day12.fs
+++ Day12.fs
@@ -2,9 +2,17 @@ module Day12
 
 open Utils
 
+type Pos = { X:int; Y:int; Z:int}
+
 let day12 = 
     print "Advent of code - Day 12 - The N-Body Problem"
 
+
+    let io = { X=1; Y=4; Z=3 }
+    let europa = { X=14; Y=9; Z=4 }
+    let ganymede = { X=4; Y=6; Z=7 }
+    let callisto = { X=6; Y=9; Z=11 }
+    
 
 
 
