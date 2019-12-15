20f4e4d84f382b50680097707f6a21e12dfeb886
Paul Brown <paul.brown@bjss.com>
2019-12-15 08:02:49 +0000

    Day 11 setup

--- Day11.fs
+++ Day11.fs
@@ -5,7 +5,8 @@ open Utils
 let day11 = 
     print "Advent of code - Day 10 - Space Police"
 
+    let prog = readCSV "./data/day9.txt" 
 
-
-
+    let o = IntCode.execute prog [| 2L |]
+    
     0
