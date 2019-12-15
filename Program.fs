3e6d3e70a7f0427db0b336a3592195c16b39a2b0
Paul Brown <paul.brown@bjss.com>
2019-12-15 07:52:46 +0000

    Day 9 - 1

--- Program.fs
+++ Program.fs
@@ -20,15 +20,11 @@ let main argv =
 
     //Day8.day8 |> ignore
     
-    // let x: bigint = bigint 34915192 * bigint 34915192
-    //printf "x : %A\n" x
-
-    //Day9.day9 |> ignore
+    Day9.day9 |> ignore
 
     //Day10.day10 |> ignore
 
-
-    Day12.day12 |> ignore
+    //Day12.day12 |> ignore
 
 
     0 // return an integer exit code
