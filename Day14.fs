5483e4d860248e93ba44b783dc1446d74202362a
Paul Brown <paul.brown@bjss.com>
2019-12-16 18:25:37 +0000

    Added days

---
+++ Day14.fs
@@ -0,0 +1,8 @@
+module Day14
+
+open Utils
+
+let day14 = 
+    print "Advent of code - Day 14 - Space Police"
+
+    //let prog = readCSV "./data/day11.txt" 
---
+++ Day16.fs
@@ -0,0 +1,8 @@
+module Day16
+
+open Utils
+
+let day16 = 
+    print "Advent of code - Day 16 - Flawed Frequency Transmission"
+
+    //let prog = readCSV "./data/day11.txt" 
