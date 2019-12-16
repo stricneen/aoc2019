ba78d7b19715f1a5d35e57dcbb3913a175d6e1c3
Paul Brown <paul.brown@bjss.com>
2019-12-16 07:16:56 +0000

    Day 12

--- Day11.fs
+++ Day11.fs
@@ -3,10 +3,21 @@ module Day11
 open Utils
 
 let day11 = 
-    print "Advent of code - Day 10 - Space Police"
+    print "Advent of code - Day 11 - Space Police"
 
-    let prog = readCSV "./data/day9.txt" 
+    let prog = readCSV "./data/day11.txt" 
 
-    let o = IntCode.execute prog [| 2L |]
+
+    let input (v:int64) = async { v }
     
+   
+   // let o = IntCode2.initialise prog input // [| 0L |]
+
+
+    System.Console.ReadKey() |> ignore
+    input 1L |> ignore
+
+    System.Console.ReadKey() |> ignore
+    input 2L |> ignore
+
     0
--- Program.fs
+++ Program.fs
@@ -17,11 +17,37 @@ let main argv =
 
     //Day8.day8 |> ignore
     
-    Day9.day9 |> ignore
+    //Day9.day9 |> ignore
 
     //Day10.day10 |> ignore
 
+    //Day11.day11 |> ignore
+
     //Day12.day12 |> ignore
+
+
+
+    let waitforit input = 
+        let signal = input
+
+        while true do
+            async {
+                printf "Waiting for input ... \n"
+                let i = signal // Should wait here unitl input func is call outside
+                printf "Input rec: %A\n" i
+            } |> ignore
+
+    let pass v = v
+
+    let wfi = waitforit (pass)
+
+    System.Console.ReadKey() |> ignore
+    pass 345 |> ignore
+
+    System.Console.ReadKey() |> ignore
+    pass 475 |> ignore
+
+
 
 
     0 // return an integer exit code
