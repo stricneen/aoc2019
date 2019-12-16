2cd16df7114dbc0887014d232c438b4d493d5742
Paul Brown <paul.brown@bjss.com>
2019-12-16 20:25:54 +0000

    Pattern

--- Day16.fs
+++ Day16.fs
@@ -9,10 +9,34 @@ let day16 =
 
     let test1 = "12345678"
 
+    let countSeq p =
+        seq {
+            yield! seq { for _ in 0 .. p -> 0 }
+            yield! seq { for _ in 0 .. p -> 1}
+            yield! seq { for _ in 0 .. p -> 0 }
+            yield! seq { for _ in 0 .. p -> -1 }
+        }
+            
+    let rec cycle xs = seq { yield! xs; yield! cycle xs }
+
+    let pattern p = cycle (countSeq p)
+
+                // for x in 0..3 do
+                //     print "S"
+
+                //inc
+           // inc
+        
 
 
+    // let pattern position = 
+    //     let p = [0, 1, 0, -1]
+    //     seq {
 
-    print prog
+    //     }
+
+
+    printf "%A\n" (pattern 4 |> Seq.skip 3 |> Seq.take 50 |>  Seq.toList)
 
 
 
