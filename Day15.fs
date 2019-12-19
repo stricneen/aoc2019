b80754f4248b4dc01f6a2dab74b03ba605c416d1
Paul Brown <paul.brown@bjss.com>
2019-12-19 14:13:00 +0000

    Maze runner

--- Day15.fs
+++ Day15.fs
@@ -14,7 +14,10 @@ let day15 =
     let inq = comp.Initialise prog
     let mutable finished = false
 
-    let mutable location = 80, 50
+    let printStatus msg = 
+        printAt 1 1 msg
+
+    let mutable location = 50, 50
     let mutable dir = Direction.North
     let wall = "#"
     let corridor = "."
@@ -27,36 +30,40 @@ let day15 =
               printAt (fst location) (snd location) "D"
               dir <-
                   match output, location with
-                  | 0L, (x,y) when dir =  Direction.North -> printAt 0 0 "Can't go N - trying E"
+                  | 0L, (x,y) when dir =  Direction.North -> printStatus "Can't go N - trying E"
                                                              printAt x (y-1) wall
                                                              Direction.East
-                  | 0L, (x,y) when dir =  Direction.East -> printAt 0 0 "Can't go E - trying S"
+                  | 0L, (x,y) when dir =  Direction.East -> printStatus "Can't go E - trying S"
                                                             printAt (x+1) y wall
                                                             Direction.South
-                  | 0L, (x,y) when dir =  Direction.South -> printAt 0 0 "Can't go S - trying W"
+                  | 0L, (x,y) when dir =  Direction.South -> printStatus "Can't go S - trying W"
                                                              printAt x (y+1) wall
                                                              Direction.West
-                  | 0L, (x,y) when dir =  Direction.West -> printAt 0 0 "Can't go W - trying N"
-                                                            printAt (x+1) y wall
+                  | 0L, (x,y) when dir =  Direction.West -> printStatus "Can't go W - trying N"
+                                                            printAt (x-1) y wall
                                                             Direction.North
-                  | 1L, (x,y) when dir =  Direction.North -> printAt 0 0 "Going N"
+                  | 1L, (x,y) when dir =  Direction.North -> printStatus "Going N     "
                                                              printAt x y corridor
                                                              location <- (x, y-1)
                                                              Direction.West
-                  | 1L, (x,y) when dir =  Direction.East -> printAt 0 0 "Going E"
+                  | 1L, (x,y) when dir =  Direction.East -> printStatus "Going E      "
                                                             printAt x y corridor
                                                             location <- (x+1, y)
                                                             Direction.North
-                  | 1L, (x,y) when dir =  Direction.South -> printAt 0 0 "Going S"
+                  | 1L, (x,y) when dir =  Direction.South -> printStatus "Going S      "
                                                              printAt x y corridor
                                                              location <- (x, y+1)
                                                              Direction.East
-                  | 1L, (x,y) when dir =  Direction.West -> printAt 0 0 "Going W"
+                  | 1L, (x,y) when dir =  Direction.West -> printStatus "Going W        "
                                                             printAt x y corridor
                                                             location <- (x-1, y)
                                                             Direction.South
+                  | 2L, (_,_) -> printStatus "************************DONE********************"
+                                 Direction.North
                   | _ -> failwith "invalid user"
-               
+
+              printAt (fst location) (snd location) "D"
+              //System.Console.ReadKey() 
               inq.Post (int64 dir)
 
          )
