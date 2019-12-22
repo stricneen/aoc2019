bf562d5f6b2a4f143eac03f508663558eb166f54
Paul Brown <paul.brown@bjss.com>
2019-12-22 00:24:31 +0000

    Parse

--- Day14.fs
+++ Day14.fs
@@ -3,12 +3,14 @@ module Day14
 open Utils
 
 type Chemical = { Name: string; Amount: int }
-type Reaction = { In: Chemical array; Out: Chemical }
+type Reaction = { In: Chemical list; Out: Chemical }
 
 let day14 = 
     print "Advent of code - Day 14 - Space Stoichiometry"
 
-    let reactions = readLines "./data/day14a.txt" 
+    let reactionsStr = readLines "./data/day14a.txt" |> Array.toList
+    let fuel = "FUEL"
+    let ore = "ORE"
 
     let parseChemical (input:string) =
         { Name = input.Substring(input.IndexOf(" ")).Trim(); 
@@ -16,14 +18,48 @@ let day14 =
 
     let parseReaction (reaction:string) =
         let inputs = reaction.Substring(0, reaction.IndexOf("=>")).Split(",")
-                     |> Array.map (fun x -> parseChemical (x.Trim())) 
-                   
-                     
+                     |> Array.toList
+                     |> List.map (fun x -> parseChemical (x.Trim())) 
         let output = reaction.Substring(reaction.IndexOf("=>") + 3).Trim()
         {
             In = inputs; Out = parseChemical output
         }
-        
+
+    let reactions = reactionsStr
+                    |> List.map parseReaction
+
+// 10 ORE => 7 A
+// 1 ORE => 1 B
+// 7 A, 1 B => 1 C
+// 7 A, 1 C => 1 D
+// 7 A, 1 D => 1 E
+// 7 A, 1 E => 1 FUEL
+
+    let withOutput chem = 
+        match chem.Name with
+        | "ORE" -> List.singleton chem
+        | _ -> (reactions |> List.find (fun x -> x.Out.Name = chem.Name)).In
+    
+    let expandReaction reaction = 
+        {
+            In = reaction.In
+                 |> (List.fold (fun a e -> List.append a (withOutput e)) [])  ;
+            Out = reaction.Out
+        }
+
+    let fuelR = reactions |> List.find(fun x -> x.Out.Name = fuel)
+   // printf "Fuel : %A\n" fuel
+
+    let rec resolve reaction = 
+        printf "%A\n" reaction
+        let exp = expandReaction reaction
+        System.Console.ReadKey() |> ignore
+        resolve exp
+
+    resolve fuelR |> ignore
+
+   // printf "Expanded : %A\n" expanded
+
 
     //let r =  "7 A, 1 D => 1 E"
     // printf "r : %A\n" (r.Substring(r.IndexOf("=>") + 3).Trim())
@@ -32,19 +68,8 @@ let day14 =
     //printf "r : %A\n" (parseChemical "7 A")
 
 
-    reactions
-    |> Array.map parseReaction
-    |> Array.iter (fun x -> printf "%A\n" x)
-
   
-  
-
-// 10 ORE => 10 A
-// 1 ORE => 1 B
-// 7 A, 1 B => 1 C
-// 7 A, 1 C => 1 D
-// 7 A, 1 D => 1 E
-// 7 A, 1 E => 1 FUEL
+    
 
 
 
