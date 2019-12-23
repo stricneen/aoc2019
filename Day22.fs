427f2f1529048655a9280acc543436de8f2db6e9
Paul Brown <paul.brown@bjss.com>
2019-12-23 20:10:29 +0000

    Day 22 - p1

--- Day22.fs
+++ Day22.fs
@@ -11,7 +11,7 @@ let day22 =
 
     let ins = readLines "./data/day22.txt" |> Array.toList
 
-    let startDeck = [ 0 .. 9 ]
+    let startDeck = [ 0 .. 10006 ]
 
     printf "%A\n" startDeck
 
@@ -34,7 +34,7 @@ let day22 =
         let mutable nw = List.init len (fun x -> 0)
         for x in 0 .. (List.length deck) - 1 do
             let pos = (x * pos) % (List.length deck)
-            printf "%A\n" pos
+            //printf "%A\n" pos
             nw <- updateEle nw pos deck.[x] 
         nw              
         
@@ -48,12 +48,23 @@ let day22 =
 
     // let ns = shuffle startDeck "deal into new stack"
     // let ns = shuffle startDeck "cut -4"
-    let ns = shuffle startDeck "deal with increment 3"
+    // let ns = shuffle startDeck "deal with increment 3"
   
+    let rec shf deck c =
+        if c > (List.length ins) - 1 then
+            deck
+        else
+            let instr = ins.[c]
+            let shuf = shuffle deck instr
+            shf shuf (c + 1)
 
 
-    printf "%A\n" ns
+    let r = shf startDeck 0
+
+    // printf "%A\n" r
   
+    let pos = r |> List.tryFindIndex(fun x -> x = 2019)
+    printf "pos : %A\n" pos
 
 
     
