42081ec5b5176e43a4f5a71317672f7d624c74ce
Paul Brown <paul.brown@bjss.com>
2019-12-23 19:55:35 +0000

    Inc

--- Day22.fs
+++ Day22.fs
@@ -25,18 +25,33 @@ let day22 =
         else
             let len = List.length deck
             (deck |> List.skip (len + pos)) @ (deck |> List.take (len + pos))
+
+    let updateEle (lst:list<int>) index itm =
+        lst.[0..index - 1] @ [ itm ] @ lst.[index + 1 ..]
+
+    let increment (deck:list<int>) (pos:int) =
+        let len = List.length deck
+        let mutable nw = List.init len (fun x -> 0)
+        for x in 0 .. (List.length deck) - 1 do
+            let pos = (x * pos) % (List.length deck)
+            printf "%A\n" pos
+            nw <- updateEle nw pos deck.[x] 
+        nw              
         
     let shuffle deck (line: string) =
         match line with
         | l when l.Contains("stack") -> deck |> List.rev
-        | l when l.Contains("increment") -> deck |> List.rev
+        | l when l.Contains("increment") -> increment deck (getNumber line)
         | l when l.Contains("cut") -> cut deck (getNumber line)
         | _ -> failwith "Unknown shuffle"
         
 
     // let ns = shuffle startDeck "deal into new stack"
-    let ns = shuffle startDeck "cut -4"
+    // let ns = shuffle startDeck "cut -4"
+    let ns = shuffle startDeck "deal with increment 3"
   
+
+
     printf "%A\n" ns
   
 
