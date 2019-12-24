23da872bf55c0d0fee6d00d324201dcc46870715
Paul Brown <paul.brown@bjss.com>
2019-12-24 09:27:27 +0000

    Day 18 - setup

--- Day18.fs
+++ Day18.fs
@@ -1,31 +1,15 @@
 module Day18
 
 open Utils
-open IntCode2
 
 let day18 = 
     print "Advent of code - Day 18 - Many-Worlds Interpretation"
 
-    let prog = readCSV "./data/day18.txt" 
-
-    let comp = IntCode2("beam")
-    let inq = comp.Initialise prog
+    let prog = readCSV "./data/day18a.txt"
 
 
-    let mutable counter = 0L
-    let mutable finished = false
 
-    comp.OutputReady.Add(fun output ->
 
-        counter <- counter + output
-
-    )    
-    
-    
-    
-    while not finished do
-        async {
-            do! Async.Sleep(100) } |> ignore
 
 
     0
