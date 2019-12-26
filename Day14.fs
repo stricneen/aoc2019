ceac7a32677c1c19c505ec2977c5674b3bdcaec8
Paul Brown <paul.brown@bjss.com>
2019-12-26 18:50:23 +0000

    Sum is correct

--- Day14.fs
+++ Day14.fs
@@ -28,33 +28,40 @@ let day14 =
     let reactions = reactionsStr
                     |> List.map parseReaction
 
-// 10 ORE => 10 A
-// 1 ORE => 1 B
-// 7 A, 1 B => 1 C
-// 7 A, 1 C => 1 D
-// 7 A, 1 D => 1 E
-// 7 A, 1 E => 1 FUEL
+    let roots = reactions
+                |> List.where (fun x -> x.In |> List.exists (fun x -> x.Name = ore))
+                |> List.map (fun x -> x.Out.Name)
 
+    // 10 ORE => 10 A
+    // 1 ORE => 1 B
+    // 7 A, 1 B => 1 C
+    // 7 A, 1 C => 1 D
+    // 7 A, 1 D => 1 E
+    // 7 A, 1 E => 1 FUEL
 
+    // 9 ORE => 2 A
+    // 8 ORE => 3 B
+    // 7 ORE => 5 C
+    // 3 A, 4 B => 1 AB
+    // 5 B, 7 C => 1 BC
+    // 4 C, 1 A => 1 CA
+    // 2 AB, 3 BC, 4 CA => 1 FUEL
 
-// 9 ORE => 2 A
-// 8 ORE => 3 B
-// 7 ORE => 5 C
-// 3 A, 4 B => 1 AB
-// 5 B, 7 C => 1 BC
-// 4 C, 1 A => 1 CA
-// 2 AB, 3 BC, 4 CA => 1 FUEL
 
     let scale chems factor = 
         chems
         |> List.map (fun x -> { x with Amount = x.Amount * factor })
+     
+    let resolve chem = 
+        let creator = reactions |> List.find (fun x -> x.Out.Name = chem.Name)
+        let factor = int(ceil((chem.Amount |> double) / (creator.Out.Amount |> double)))
+        scale creator.In factor
 
     let withOutput chem = 
-        match chem.Name with
-        | "ORE" -> List.singleton chem
-        | _ -> let creator = reactions |> List.find (fun x -> x.Out.Name = chem.Name)
-               let factor = int(ceil((chem.Amount |> double) / (creator.Out.Amount |> double)))
-               scale creator.In factor
+        if roots |> List.exists (fun x -> x = chem.Name) then
+            List.singleton chem
+        else
+           resolve chem
 
     let simplify chems =
         chems
@@ -65,11 +72,9 @@ let day14 =
                                  { chem with Amount = chem.Amount + e.Amount } ::
                                   (a |> List.where(fun x -> x.Name <> chem.Name))) []
 
-
     let expandReaction reaction = 
         let chems = reaction.In
                     |> (List.fold (fun a e -> List.append a (withOutput e)) [])
-        
         printf "%A\n" chems
         printf "%A\n" (simplify chems)
         {
@@ -79,20 +84,30 @@ let day14 =
 
     let fuelR = reactions |> List.find(fun x -> x.Out.Name = fuel)
 
-    let rec resolve reaction = 
-        if reaction.In |> List.forall(fun x -> x.Name = ore) then 
+    let rec loop reaction = 
+        if reaction.In |> List.forall(fun x -> roots |> List.contains x.Name) then 
             reaction
         else
-            
             let exp = expandReaction reaction
-            // System.Console.ReadKey() |> ignore
-            resolve exp
+            loop exp
 
-    let o = resolve fuelR
+    let o = loop fuelR
 
     printf "Done : %A\n" o
+    // let r = reactions
+    //         |> List.where (fun x -> x.In |> List.exists (fun x -> x.Name = ore))
 
-    printf "In ORE : %A\n" (o.In |> List.sumBy (fun x -> x.Amount))
+    let t = o.In |> List.map resolve
+    
+    printf "res : %A\n" t
+
+
+
+
+    let s = t |> List.map List.head
+    printf "s : %A\n" s
+
+    printf "In ORE : %A\n" (s |> List.sumBy (fun x -> x.Amount))
 
 
 
