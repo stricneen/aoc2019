32cf481a999cb302331555134f0fa5730853a552
Paul Brown <paul.brown@bjss.com>
2020-01-23 15:27:35 +0000

    Allowed for rectangular mazes

--- Day20.fs
+++ Day20.fs
@@ -28,35 +28,55 @@ let day20 =
         let removeDots s = 
             s |> Seq.where(fun x -> x <> '.') |> String.Concat
 
-        let rec loop c res =
+        let rec rows c res = 
             if c > Array2D.length1 donut - 1 then
                 res
             else
                 let rowC = donut.[c, *]
-                let colC = donut.[*, c]
+         //       let colC = donut.[*, c]
                 
                 let row = getPairs rowC
-                let col = getPairs colC
+               // let col = getPairs colC
 
                 let rowCrds = row |> Array.map(fun x -> let offset = if x.[0] = '.' then 0 else 3
                                                         let ind = (rowC |> String).IndexOf x + offset
                                                         removeDots x, (ind, c))
+                // let colCrds = col |> Array.map(fun x -> let offset = if x.[0] = '.' then 0 else 3
+                //                                         let ind = (colC |> String).IndexOf x + offset
+                //                                         removeDots x, (c, ind ))
+                
+              //   let rc = Array.append rowCrds 
+                rows (c+1) (Array.append res rowCrds)
+
+
+        let rec cols c res =
+            if c > Array2D.length2 donut - 1 then
+                res
+            else
+                //let rowC = donut.[c, *]
+                let colC = donut.[*, c]
+                
+                //let row = getPairs rowC
+                let col = getPairs colC
+
+                // let rowCrds = row |> Array.map(fun x -> let offset = if x.[0] = '.' then 0 else 3
+                //                                         let ind = (rowC |> String).IndexOf x + offset
+                //                                         removeDots x, (ind, c))
                 let colCrds = col |> Array.map(fun x -> let offset = if x.[0] = '.' then 0 else 3
                                                         let ind = (colC |> String).IndexOf x + offset
                                                         removeDots x, (c, ind ))
                 
-                let rc = Array.append rowCrds colCrds
-                loop (c+1) (Array.append res rc)
+            //    let rc = Array.append rowCrds colCrds
+                cols (c+1) (Array.append res colCrds)
 
-
-        loop 0 [||]
+        Array.append (rows 0 [||]) (cols 0 [||])
         
 
 
 
+    let nodes = getNodes 
 
-
-    printf "%A\n" getNodes
+    printf "%A\n" nodes
 
 
     // let mutable finished = false
