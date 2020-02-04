e0ccdfd376aeb3acd1671e19a220b42ffe878dc3
Paul Brown <paul.brown@bjss.com>
2020-02-04 22:16:24 +0000

    Day 17 lang

--- Day17.fs
+++ Day17.fs
@@ -25,47 +25,76 @@ let day17 =
                         yield x * y
     }
         
-
-
-
-
-    let mutable counter = 0L
     let mutable finished = false
-    let mutable collecting = false
 
     let scaffold = Array2D.create 85 85 ' '
 
-    let mutable x = 0
-    let mutable y = 0
 
-    // let completed = 
-    //     printf "tt: %A\n" scaffold
-    
-    comp.OutputReady.Add(fun output ->
+    let mutable buffer = ""
 
+    comp.OutputReady.Add(fun output -> 
         if output = 10L then
-            let row = scaffold.[*, y] |> String
-            print row
-            
-            x <- 0
-            y <- y + 1
-            // printn y
-            
-
-            if y = 42 then
-                print "done yay!"
-                //printf "tt: %A\n" scaffold
-                let o = findIntersection scaffold
-                        |> Seq.sum
-                printn o
-                finished <- true
-                //completed
+            print buffer
+            //print "\n"
+            buffer <- ""
         else
-            Array2D.set scaffold x y (char output) 
-            x <- x + 1
+            buffer <- buffer + (char(output |> int)).ToString()
+        )
 
-    )    
+
+
+    //let mutable x = 0
+    //let mutable y = 0
+    // comp.OutputReady.Add(fun output ->
+    //     DAY 1
+    //     if output = 10L then
+    //         let row = scaffold.[*, y] |> String
+    //         print row   
+    //         x <- 0
+    //         y <- y + 1
+    //         if y = 42 then
+    //             print "done yay!"
+    //             let o = findIntersection scaffold
+    //                     |> Seq.sum
+    //             printn o
+    //             finished <- true
+    //     else
+    //         Array2D.set scaffold x y (char output) 
+    //         x <- x + 1
+    // )    
+
+    let toIn commands = 
+
+        let inline charToInt c = int64 c
+        print commands
+        print ""
+
+        commands//.Split(',')
+        |> Seq.iter(fun x ->
+            let x' = charToInt x
+
+            printf "%A\n" x'
+            inq.Post x'
+            //inq.Post 44L // comma
+        )
+        printf "%A\n" 10L
+        inq.Post 10L // newline
+
     
+    // Main routine: A,B,C,B,A,C
+    // (ASCII input: 65, 44, 66, 44, 67, 44, 66, 44, 65, 44, 67, 10)
+    // Function A:   R,8,R,8
+    // (ASCII input: 82, 44, 56, 44, 82, 44, 56, 10)
+    // Function B:   R,4,R,4,R,8
+    // (ASCII input: 82, 44, 52, 44, 82, 44, 52, 44, 82, 44, 56, 10)
+    // Function C:   L,6,L,2
+    // (ASCII input: 76, 44, 54, 44, 76, 44, 50, 10)
+    Console.ReadKey() |> ignore
+    "A,A" |> toIn
+    "R,6,L,10,R,9,R,8,R,1" |> toIn
+    "R" |> toIn
+    "R" |> toIn
+    "n" |> toIn
 
 
    
