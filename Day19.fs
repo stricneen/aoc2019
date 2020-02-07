3afb5e3c7ecf958dc3497d66505c39b79b404590
Paul Brown <paul.brown@bjss.com>
2020-02-07 21:07:27 +0000

    Day 20

--- Day19.fs
+++ Day19.fs
@@ -30,37 +30,39 @@ let day19 =
         o = 1L
 
 
-    // let mutable count = 0
-    // for y in 0L .. 49L do
-    //     for x in 0L .. 49L do
-    //         let (x,y), drone = isInField x y
-    //         if drone = 1L then
-    //             count <- count + 1
-    //         //printAt (int x) (int y) (if drone = 0L then "." else "#")     
-    // print "Part 1"
-    // pl count      
+    let mutable count = 0
+    for y in 0L .. 49L do
+        for x in 0L .. 49L do
+            let (x,y), drone = isInField x y
+            if drone = 1L then
+                count <- count + 1
+            printAt (int x) (int y) (if drone = 0L then "." else "#")     
+    print "Part 1"
+    pl count      
 
     let hasOppositeCorner x y s =
-        inField (x-s) (y+s)
+        let delta = s - 1L
+        inField (x-delta) (y+delta)
         
     let start x y s =
 
         let rec loop x y =
             if not (inField x y) then
-                //printf "Not in field %A %A\n" x y
+               // printf "Not in field %A %A\n" x y
                 loop x (y+1L)
             else
                 if hasOppositeCorner x y s then
-                    printf "Found %A @ %A,%A\n" s (x-s) y
+                    printf "Found %A @ %A,%A\n" s x y
                     s
                 else 
                     loop (x+1L) y
-                    
-            
-
         loop x y
 
-    start 10L 10L 100L
+    start 10L 10L 1L |> ignore
+    start 10L 10L 2L |> ignore
+    start 10L 10L 3L |> ignore
+    start 10L 10L 4L |> ignore
+    start 10L 10L 5L |> ignore
 
     while not finished do
         async { do! Async.Sleep(100) } |> ignore
