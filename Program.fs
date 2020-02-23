22b7e425562533ed4c8c04dd729e29396a287bd2
Paul Brown <paul.brown@bjss.com>
2020-02-23 22:42:11 +0000

    Day 21

--- Program.fs
+++ Program.fs
@@ -18,10 +18,10 @@ let main argv =
     //Day15.day15 |> ignore
     //Day16.day16 |> ignore
     //Day17.day17 |> ignore
-    Day18.day18 |> ignore
+    //Day18.day18 |> ignore
     //Day19.day19 |> ignore
     //Day20.day20 |> ignore
-    //Day21.day21 |> ignore
+    Day21.day21 |> ignore
     //Day22.day22 |> ignore
     //Day24.day24 |> ignore
     //Day25.day25 |> ignore
